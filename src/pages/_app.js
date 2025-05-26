648f5ba0a683be7152cf4ece2d7e46def899ce47
Vipul Mehra <vipulmehra02899@gmail.com>
2025-05-26 16:11:19 +0530

    Updated favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
-import "@/styles/globals.css";
+import '../styles/globals.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Vipul- Personal Dashboard</title>
+        <link rel="icon" href="/image.png" />
+        <meta name="description" content="Personal dashboard of Vipul Mehra, showcasing academic and professional details." />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
